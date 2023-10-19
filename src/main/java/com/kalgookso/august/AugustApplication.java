c489c5cf62cbc25179602d7ca79c51d430cfe06a
김도엽 <miro3721@plani.co.kr>
2023-10-19 20:25:37 +0900

    애플리케이션 구동 시 테스트 계정 셋업 추가

--- src/main/java/com/kalgookso/august/AugustApplication.java
+++ src/main/java/com/kalgookso/august/AugustApplication.java
@@ -1,15 +1,30 @@
 package com.kalgookso.august;
 
+import com.kalgookso.august.command.AccountCommand;
+import com.kalgookso.august.service.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class AugustApplication {
+public class AugustApplication implements CommandLineRunner {
+
+    private final AccountService accountService;
+
+    public AugustApplication(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AugustApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        AccountCommand.Post command = new AccountCommand.Post("tester", "1234", "관리자");
+        this.accountService.create(command);
     }
 
 }
