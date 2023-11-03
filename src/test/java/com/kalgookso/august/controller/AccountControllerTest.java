d9ef699fec2edd6c8e0758e08fc987bfd93a2e5c
김도엽 <miro3721@plani.co.kr>
2023-11-03 17:40:21 +0900

    AccountControllerTest 수정

--- src/test/java/com/kalgookso/august/controller/AccountControllerTest.java
+++ src/test/java/com/kalgookso/august/controller/AccountControllerTest.java
@@ -1,19 +1,15 @@
 package com.kalgookso.august.controller;
 
-import com.kalgookso.august.entity.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.Collections;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,10 +22,6 @@ class AccountControllerTest {
     @Test
     @DisplayName("계정 목록 페이지를 반환합니다.")
     void getAll() throws Exception {
-
-        // 가짜 데이터 생성
-        Account account = new Account("test", "test", "test");
-        PageImpl<Account> accounts = new PageImpl<>(Collections.singletonList(account));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/accounts"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
