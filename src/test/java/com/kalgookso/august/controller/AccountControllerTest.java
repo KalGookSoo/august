aaba77d50db7242e1043910efab8a5f6e631102c
김도엽 <miro3721@plani.co.kr>
2023-11-23 18:09:41 +0900

    feat: AccountController 테스트 코드 추가

--- src/test/java/com/kalgookso/august/controller/AccountControllerTest.java
+++ src/test/java/com/kalgookso/august/controller/AccountControllerTest.java
@@ -18,16 +18,74 @@ class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @WithMockUser(username = "test", password = "test", roles = "USER")
     @Test
-    @DisplayName("계정 목록 페이지를 반환합니다.")
+    @DisplayName("계정 목록이 담긴 뷰를 반환합니다.")
+    @WithMockUser(username = "test", password = "test", roles = "USER")
     void getAll() throws Exception {
-
         mockMvc.perform(MockMvcRequestBuilders.get("/accounts"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("accounts/list"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("pagedModel"))
                 .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("계정 생성을 위한 뷰를 반환합니다.")
+    @WithMockUser(username = "test", password = "test", roles = "USER")
+    void getNew() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/accounts/new"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("accounts/new"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("계정 정보가 담긴 뷰를 반환합니다.")
+    @WithMockUser(username = "test", password = "test", roles = "USER")
+    void getOne() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("계정을 생성합니다.")
+    @WithMockUser(username = "test", password = "test", roles = "USER")
+    void create() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("계정 정보를 수정하기 위한 뷰를 반환합니다.")
+    @WithMockUser(username = "test", password = "test", roles = "USER")
+    void getEdit() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("계정 정보를 수정합니다.")
+    @WithMockUser(username = "test", password = "test", roles = "USER")
+    void update() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("계정을 삭제합니다.")
+    @WithMockUser(username = "test", password = "test", roles = "USER")
+    void delete() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("계정 패스워드를 수정하기 위한 뷰를 반환합니다.")
+    @WithMockUser(username = "test", password = "test", roles = "USER")
+    void getEditPassword() throws Exception {
+
+    }
+
+    @Test
+    @DisplayName("계정 패스워드를 수정합니다.")
+    @WithMockUser(username = "test", password = "test", roles = "USER")
+    void updatePassword() throws Exception {
+
     }
 
 }
