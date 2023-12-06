31ac4d938e48a7803bdbbe38d52769b5e10f0373
김도엽 <miro3721@plani.co.kr>
2023-12-06 23:02:41 +0900

    refactor: Remove AttachmentRepository and CommentRepository
    - Delete AttachmentRepository.java and CommentRepository.java
    - The management of Attachment and Comment is now handled through the Article entity

--- src/main/java/com/kalgookso/august/repository/AttachmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kalgookso.august.repository;
-
-import com.kalgookso.august.entity.article.Attachment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface AttachmentRepository extends JpaRepository<Attachment, String>, JpaSpecificationExecutor<Attachment> {
-}
--- src/main/java/com/kalgookso/august/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kalgookso.august.repository;
-
-import com.kalgookso.august.entity.article.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CommentRepository extends JpaRepository<Comment, String>, JpaSpecificationExecutor<Comment> {
-}
