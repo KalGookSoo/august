27f3f80ab43ca1d930ae8254853690ed2ba17fe9
김도엽 <miro3721@plani.co.kr>
2024-01-12 17:53:49 +0900

    refactor: switch H2 database from file-based to in-memory and enhance SQL script execution

--- src/main/java/com/kalgookso/august/AugustApplication.java
+++ src/main/java/com/kalgookso/august/AugustApplication.java
@@ -4,49 +4,48 @@ import com.kalgookso.august.entity.account.Account;
 import com.kalgookso.august.entity.account.Authority;
 import com.kalgookso.august.exception.UsernameAlreadyExistsException;
 import com.kalgookso.august.service.AccountService;
+import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 /**
- * AugustApplication 클래스입니다.
  * 이 클래스는 스프링 부트 애플리케이션의 진입점입니다.
  */
 @SpringBootApplication
 @EnableAsync
 public class AugustApplication implements CommandLineRunner {
 
-    private final AccountService accountService;  // 계정 서비스
+    private final DataSource dataSource;
+
+    private final AccountService accountService;
 
     private final Logger logger = LoggerFactory.getLogger(AugustApplication.class);
 
-    /**
-     * AugustApplication 생성자입니다.
-     * @param accountService 계정 서비스
-     */
-    public AugustApplication(AccountService accountService) {
+    public AugustApplication(DataSource dataSource, AccountService accountService) {
+        this.dataSource = dataSource;
         this.accountService = accountService;
     }
 
-    /**
-     * 애플리케이션의 메인 메서드입니다.
-     * @param args 커맨드라인 인자
-     */
     public static void main(String[] args) {
         SpringApplication.run(AugustApplication.class, args);
     }
 
-    /**
-     * 애플리케이션 실행 후 호출되는 메서드입니다.
-     * 이 메서드에서는 테스트 계정을 생성하고 저장합니다.
-     * @param args 커맨드라인 인자
-     */
     @Override
     public void run(String... args) {
+        if (((HikariDataSource) dataSource).getDriverClassName().contains("h2")) {
+            executeSqlScript();
+        }
         Account account = new Account();
         account.setUsername("admin");
         account.setPassword("1234");
@@ -56,6 +55,15 @@ public class AugustApplication implements CommandLineRunner {
             this.accountService.create(account);
         } catch (UsernameAlreadyExistsException e) {
             logger.info("Username already exists");
+        }
+    }
+
+    private void executeSqlScript() {
+        Resource resource = new ClassPathResource("scripts/createAclSchema.sql");
+        try {
+            ScriptUtils.executeSqlScript(dataSource.getConnection(), resource);
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to execute SQL script: " + e.getMessage(), e);
         }
     }
 
